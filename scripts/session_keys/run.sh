7a2120108d7600e9c4e9929b17690d3824c3619c
imstar15 <imstar15@qq.com>
2021-11-16 23:59:16 +0800

    Modify run.sh

--- scripts/session_keys/run.sh
+++ scripts/session_keys/run.sh
@@ -1,19 +1,24 @@
 mnemonic_phrase="<mnemonic_phrase>"
-validator1_rpc_url="<validator1_rpc_url>"
-validator2_rpc_url="<validator2_rpc_url>"
-validator3_rpc_url="<validator3_rpc_url>"
+validator_rpc_urls=("validator1_rpc_url" "<validator2_rpc_url>" "<validator3_rpc_url>")
 
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"audi\", \"${mnemonic_phrase}//1//authority_discovery\", \"0x<authority_discovery public key (hex)>\"]}" ${validator1_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"babe\", \"${mnemonic_phrase}//1//babe\", \"0x<babe public key (hex)>\"]}" ${validator1_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"gran\", \"${mnemonic_phrase}//1//grandpa\", \"0x<grandpa public key (hex)>\"]}" ${validator1_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"imon\", \"${mnemonic_phrase}//1//im_online\", \"0x<im_online public key (hex)>\"]}" ${validator1_rpc_url}
+insert_key(){
+  curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"${2}\", \"${mnemonic_phrase}//${1}//${3}\", \"0x${4}\"]}" ${validator_rpc_urls[0]}
+}
 
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"audi\", \"${mnemonic_phrase}//2//authority_discovery\", \"0x<authority_discovery public key (hex)>\"]}" ${validator2_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"babe\", \"${mnemonic_phrase}//2//babe\", \"0x<babe public key (hex)>\"]}" ${validator2_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"gran\", \"${mnemonic_phrase}//2//grandpa\", \"0x<grandpa public key (hex)>\"]}" ${validator2_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"imon\", \"${mnemonic_phrase}//2//im_online\", \"0x<im_online public key (hex)>\"]}" ${validator2_rpc_url}
+# Validator1
+insert_key 1 'audi' 'authority_discovery' '<authority_discovery public key (hex)>'
+insert_key 1 'babe' 'babe' '<babe public key (hex)>'
+insert_key 1 'grandpa' 'grandpa' '<grandpa public key (hex)>'
+insert_key 1 'imon' 'im_online' '<im_online public key (hex)>'
 
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"audi\", \"${mnemonic_phrase}//3//authority_discovery\", \"0x<authority_discovery public key (hex)>\"]}" ${validator3_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"babe\", \"${mnemonic_phrase}//3//babe\", \"0x<babe public key (hex)>\"]}" ${validator3_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"gran\", \"${mnemonic_phrase}//3//grandpa\", \"0x<grandpa public key (hex)>\"]}" ${validator3_rpc_url}
-curl -H "Content-Type: application/json" -d "{\"id\":1, \"jsonrpc\":\"2.0\", \"method\": \"author_insertKey\", \"params\":[\"imon\", \"${mnemonic_phrase}//3//im_online\", \"0x<im_online public key (hex)>\"]}" ${validator3_rpc_url}
+# Validator2
+insert_key 2 'audi' 'authority_discovery' '<authority_discovery public key (hex)>'
+insert_key 2 'babe' 'babe' '<babe public key (hex)>'
+insert_key 2 'grandpa' 'grandpa' '<grandpa public key (hex)>'
+insert_key 2 'imon' 'im_online' '<im_online public key (hex)>'
+
+# Validator3
+insert_key 3 'audi' 'authority_discovery' '<authority_discovery public key (hex)>'
+insert_key 3 'babe' 'babe' '<babe public key (hex)>'
+insert_key 3 'grandpa' 'grandpa' '<grandpa public key (hex)>'
+insert_key 3 'imon' 'im_online' '<im_online public key (hex)>'
