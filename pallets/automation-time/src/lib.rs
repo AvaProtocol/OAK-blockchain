02cb86c13d20308b6d982b2a648c6cbca0594bac
Ryan Huttman <ryan@oak.tech>
2022-01-07 21:17:26 -0500

    check to see if a time is valid

--- pallets/automation-time/src/lib.rs
+++ pallets/automation-time/src/lib.rs
@@ -24,14 +24,20 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 pub use pallet::*;
+use pallet_timestamp::{self as timestamp};
+use sp_runtime:: {
+	traits::{SaturatedConversion},
+};
 
 #[cfg(feature = "runtime-benchmarks")]
 mod benchmarking;
 
 #[frame_support::pallet]
 pub mod pallet {
+	use super::*;
+
 	#[pallet::config]
-	pub trait Config: frame_system::Config {}
+	pub trait Config: frame_system::Config + pallet_timestamp::Config {}
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
@@ -39,9 +45,43 @@ pub mod pallet {
 
 	#[pallet::error]
 	pub enum Error<T> {
-		Placeholder,
+		/// Time must end in a whole minute.
+		InvalidTime,
+		/// Time must be in the future.
+		PastTime,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {}
+
+	impl<T: Config> Pallet<T> {
+		
+		/// Get the relevant time slot.
+		/// 
+		/// In order to do this we get the most recent timestamp from the chain. Then convert
+		/// the ms unix timestamp to seconds. Lastly, we bring the timestamp down to the last whole minute.
+		fn get_time_slot() -> u64 {
+			let now = <timestamp::Pallet<T>>::get().saturated_into::<u64>();
+			let now = now / 1000;
+			let diff_to_min = now % 60;
+			now - diff_to_min
+		}
+
+		/// Checks to see if the scheduled time is a valid timestamp.
+		/// 
+		/// In order for a time to be valid it must end in a whole minute and be in the future.
+		fn is_valid_time(scheduled_time: u64) -> Result<(), Error<T>> {
+			let remainder = scheduled_time % 60;
+			if remainder != 0 {
+				Err(<Error<T>>::InvalidTime)?;
+			}
+		
+			let now = Self::get_time_slot();
+			if scheduled_time <= now  {
+				Err(<Error<T>>::PastTime)?;
+			}
+
+			Ok(())
+		}
+	}
 }
