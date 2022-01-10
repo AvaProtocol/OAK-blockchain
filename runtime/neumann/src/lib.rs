31d1f43b2f74d34f8a16f8dc369e26b34c61d05f
imstar15 <imstar15@qq.com>
2022-01-10 13:18:43 +0800

    Upgrade runtime to 271

--- runtime/neumann/src/lib.rs
+++ runtime/neumann/src/lib.rs
@@ -180,7 +180,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("neumann"),
 	impl_name: create_runtime_str!("neumann"),
 	authoring_version: 1,
-	spec_version: 270,
+	spec_version: 271,
 	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 1,
