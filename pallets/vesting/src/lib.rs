9e68e9833637007ded02b4d23b5f12f984fd2fea
Ryan Huttman <ryan@oak.tech>
2022-02-28 13:06:13 -0800

    Vesting pallet (#88)
    * vesting pallet
    
    * fmt

---
+++ pallets/vesting/src/lib.rs
@@ -0,0 +1,153 @@
+// This file is part of OAK Blockchain.
+
+// Copyright (C) 2021 OAK Network
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! # Vesting pallet
+//!
+//! This pallet will mint tokens to the designated accounts at the designated times.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(test)]
+mod mock;
+#[cfg(test)]
+mod tests;
+
+use frame_support::pallet;
+pub use pallet::*;
+
+#[pallet]
+pub mod pallet {
+	use sp_runtime::traits::SaturatedConversion;
+	use sp_std::vec::Vec;
+
+	use frame_support::{pallet_prelude::*, traits::Currency};
+	use frame_system::pallet_prelude::*;
+	use pallet_timestamp::{self as timestamp};
+
+	type AccountOf<T> = <T as frame_system::Config>::AccountId;
+	type BalanceOf<T> = <<T as Config>::Currency as Currency<AccountOf<T>>>::Balance;
+	type UnixTime = u64;
+
+	#[pallet::config]
+	pub trait Config: frame_system::Config + pallet_timestamp::Config {
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// The Currency handler
+		type Currency: Currency<Self::AccountId>;
+	}
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(crate) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// A vest occured.
+		Vested {
+			account: AccountOf<T>,
+			amount: BalanceOf<T>,
+		},
+		VestFailed {
+			account: AccountOf<T>,
+			amount: BalanceOf<T>,
+			error: DispatchError,
+		},
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Block time not set.
+		BlockTimeNotSet,
+	}
+
+	#[pallet::pallet]
+	#[pallet::without_storage_info]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// The closed pallet map. Each pallet in here will not receive transcations.
+	#[pallet::storage]
+	#[pallet::getter(fn get_scheduled_vest)]
+	pub type VestingSchedule<T: Config> =
+		StorageMap<_, Twox64Concat, UnixTime, Vec<(AccountOf<T>, BalanceOf<T>)>, OptionQuery>;
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		fn on_initialize(_: T::BlockNumber) -> Weight {
+			Self::vest();
+			0
+		}
+	}
+
+	impl<T: Config> Pallet<T> {
+		/// Based on the block time, return the time slot.
+		///
+		/// In order to do this we:
+		/// * Get the most recent timestamp from the block.
+		/// * Convert the ms unix timestamp to seconds.
+		/// * Bring the timestamp down to the last whole hour.
+		fn get_current_time_slot() -> Result<UnixTime, Error<T>> {
+			let now = <timestamp::Pallet<T>>::get().saturated_into::<UnixTime>();
+			if now == 0 {
+				Err(Error::<T>::BlockTimeNotSet)?
+			}
+			let now = now / 1000;
+			let diff_to_min = now % 3600;
+			Ok(now - diff_to_min)
+		}
+
+		/// Mint tokens for any accounts that have vested.
+		pub fn vest() {
+			if let Ok(current_time) = Self::get_current_time_slot() {
+				if let Some(scheduled) = Self::get_scheduled_vest(current_time) {
+					for (account, amount) in scheduled {
+						<T as Config>::Currency::deposit_creating(&account, amount);
+						Self::deposit_event(Event::Vested { account, amount })
+					}
+				}
+				VestingSchedule::<T>::remove(current_time);
+			}
+		}
+	}
+
+	#[pallet::genesis_config]
+	pub struct GenesisConfig<T: Config> {
+		pub vesting_schedule: Vec<(UnixTime, Vec<(AccountOf<T>, BalanceOf<T>)>)>,
+	}
+
+	#[cfg(feature = "std")]
+	impl<T: Config> Default for GenesisConfig<T> {
+		fn default() -> Self {
+			Self { vesting_schedule: Default::default() }
+		}
+	}
+
+	#[pallet::genesis_build]
+	impl<T: Config> GenesisBuild<T> for GenesisConfig<T> {
+		fn build(&self) {
+			for (time, schedule) in self.vesting_schedule.iter() {
+				assert!(time % 3600 == 0, "Invalid time");
+				let mut scheduled_vests: Vec<(AccountOf<T>, BalanceOf<T>)> = vec![];
+				for (account, amount) in schedule {
+					assert!(
+						*amount > <T>::Currency::minimum_balance(),
+						"Cannot vest less than the existential deposit"
+					);
+					scheduled_vests.push((account.clone(), amount.clone()));
+				}
+				VestingSchedule::<T>::insert(time, scheduled_vests);
+			}
+		}
+	}
+}
---
+++ pallets/vesting/src/mock.rs
@@ -0,0 +1,165 @@
+// This file is part of OAK Blockchain.
+
+// Copyright (C) 2022 OAK Network
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use super::*;
+use crate as pallet_vesting;
+use frame_support::{
+	construct_runtime, parameter_types,
+	traits::{Everything, GenesisBuild},
+	weights::Weight,
+};
+use sp_core::H256;
+use sp_runtime::{
+	testing::Header,
+	traits::{BlakeTwo256, IdentityLookup},
+	Perbill,
+};
+
+pub type AccountId = u64;
+pub type BlockNumber = u64;
+pub type Balance = u128;
+
+pub const ALICE: AccountId = 1;
+pub const BOB: AccountId = 2;
+
+type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
+type Block = frame_system::mocking::MockBlock<Test>;
+
+construct_runtime!(
+	pub enum Test where
+		Block = Block,
+		NodeBlock = Block,
+		UncheckedExtrinsic = UncheckedExtrinsic,
+	{
+		System: frame_system::{Pallet, Call, Config, Storage, Event<T>},
+		Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
+		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+		Vesting: pallet_vesting::{Pallet, Storage, Config<T>, Event<T>},
+	}
+);
+
+parameter_types! {
+	pub const BlockHashCount: u64 = 250;
+	pub const MaximumBlockWeight: Weight = 1024;
+	pub const MaximumBlockLength: u32 = 2 * 1024;
+	pub const AvailableBlockRatio: Perbill = Perbill::one();
+	pub const SS58Prefix: u8 = 51;
+}
+impl frame_system::Config for Test {
+	type BaseCallFilter = Everything;
+	type DbWeight = ();
+	type Origin = Origin;
+	type Index = u64;
+	type BlockNumber = BlockNumber;
+	type Call = Call;
+	type Hash = H256;
+	type Hashing = BlakeTwo256;
+	type AccountId = AccountId;
+	type Lookup = IdentityLookup<Self::AccountId>;
+	type Header = Header;
+	type Event = Event;
+	type BlockHashCount = BlockHashCount;
+	type Version = ();
+	type PalletInfo = PalletInfo;
+	type AccountData = pallet_balances::AccountData<Balance>;
+	type OnNewAccount = ();
+	type OnKilledAccount = ();
+	type SystemWeightInfo = ();
+	type BlockWeights = ();
+	type BlockLength = ();
+	type SS58Prefix = SS58Prefix;
+	type OnSetCode = ();
+	type MaxConsumers = frame_support::traits::ConstU32<16>;
+}
+
+parameter_types! {
+	pub const ExistentialDeposit: u64 = 10;
+	pub const MaxLocks: u32 = 50;
+	pub const MaxReserves: u32 = 50;
+}
+
+impl pallet_balances::Config for Test {
+	type MaxLocks = MaxLocks;
+	type Balance = Balance;
+	type Event = Event;
+	type DustRemoval = ();
+	type ExistentialDeposit = ExistentialDeposit;
+	type AccountStore = System;
+	type WeightInfo = ();
+	type MaxReserves = MaxReserves;
+	type ReserveIdentifier = [u8; 8];
+}
+
+parameter_types! {
+	pub const MinimumPeriod: u64 = 1000;
+}
+
+impl pallet_timestamp::Config for Test {
+	type Moment = u64;
+	type OnTimestampSet = ();
+	type MinimumPeriod = MinimumPeriod;
+	type WeightInfo = ();
+}
+
+impl Config for Test {
+	type Event = Event;
+	type Currency = Balances;
+}
+
+pub(crate) struct ExtBuilder {
+	vesting_schedule: Vec<(u64, Vec<(AccountId, Balance)>)>,
+}
+
+impl Default for ExtBuilder {
+	fn default() -> ExtBuilder {
+		ExtBuilder { vesting_schedule: Default::default() }
+	}
+}
+
+impl ExtBuilder {
+	pub(crate) fn schedule(mut self, v: Vec<(u64, Vec<(AccountId, Balance)>)>) -> Self {
+		self.vesting_schedule = v;
+		self
+	}
+
+	pub(crate) fn build(self) -> sp_io::TestExternalities {
+		let mut t = frame_system::GenesisConfig::default()
+			.build_storage::<Test>()
+			.expect("Frame system builds valid default genesis config");
+
+		GenesisBuild::<Test>::assimilate_storage(
+			&pallet_vesting::GenesisConfig { vesting_schedule: self.vesting_schedule },
+			&mut t,
+		)
+		.expect("Pallet valve vesting can be assimilated");
+
+		let mut ext = sp_io::TestExternalities::new(t);
+		ext.execute_with(|| System::set_block_number(1));
+		ext
+	}
+}
+
+pub(crate) fn events() -> Vec<pallet_vesting::Event<Test>> {
+	let evt = System::events()
+		.into_iter()
+		.map(|r| r.event)
+		.filter_map(|e| if let Event::Vesting(inner) = e { Some(inner) } else { None })
+		.collect::<Vec<_>>();
+
+	System::reset_events();
+	evt
+}
