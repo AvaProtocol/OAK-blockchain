69fd5a790394167a0ea7e41745f64f23a04bab3e
Justin Zhou <justinzhou@macbook-pro.myfiosgateway.com>
2022-03-04 11:07:10 -0500

    fix compilation, tested locally

--- pallets/automation-time/src/benchmarking.rs
+++ pallets/automation-time/src/benchmarking.rs
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// #![cfg(feature = "runtime-benchmarks")]
+#![cfg(feature = "runtime-benchmarks")]
 
 use super::*;
 use frame_benchmarking::{account, benchmarks};
