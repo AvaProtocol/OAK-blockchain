831c4cf2ab6aa9e6782f6da8ef5a265732200328
irsal <irsal@users.noreply.github.com>
2022-03-10 21:04:08 -0600

    Release prep for 1.2.7 && 276 runtime  (#100)
    * Prep 1.2.7 && 276 release
    
    * add turing docker files
    
    * rename release-v0.9.17 to polkadot-v0.9.17
    
    * Revert "rename release-v0.9.17 to polkadot-v0.9.17"
    
    This reverts commit 7f38d153ea154a769e145f51709f9ee75a2262ae.
    
    * new lock
    
    * try transaction_version
    
    * cl
    
    * same authoring version for turing

--- runtime/neumann/src/lib.rs
+++ runtime/neumann/src/lib.rs
@@ -176,10 +176,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("neumann"),
 	impl_name: create_runtime_str!("neumann"),
 	authoring_version: 1,
-	spec_version: 275,
+	spec_version: 276,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 3,
+	transaction_version: 4,
 	state_version: 0,
 };
 
