d6588a3dc372c7060268fd9fe622aafda4c215d0
Justin Zhou <justinzhou@macbook-pro.myfiosgateway.com>
2022-04-14 12:00:22 -0400

    add last time slot changes

--- pallets/automation-time/src/migrations.rs
+++ pallets/automation-time/src/migrations.rs
@@ -25,9 +25,9 @@ pub mod v1 {
 }
 
 pub mod v2 {
-	use frame_support::{migration::{storage_iter, storage_key_iter}, traits::StorageVersion, Twox64Concat, BoundedVec};
+	use frame_support::{migration::{storage_iter, storage_key_iter, get_storage_value}, traits::StorageVersion, Twox64Concat, BoundedVec};
 
-	use crate::{Pallet, Task, Vec};
+	use crate::{LastTimeSlot, Pallet, Task, Vec};
 
 	use super::*;
 
@@ -52,6 +52,14 @@ pub mod v2 {
 		let _scheduled_tasks: Vec<_> = storage_key_iter::<u64, BoundedVec<T::Hash, T::MaxTasksPerSlot>, Twox64Concat>(pallet_prefix, scheduled_tasks_prefix)
 			.drain()
 			.collect();
+
+		let pallet_prefix: &[u8] = b"AutomationTime";
+		let storage_item_prefix: &[u8] = b"LastTimeSlot";
+		let stored_min_slot = get_storage_value::<u64>(pallet_prefix, storage_item_prefix, &[])
+			.expect("Must have last slot value");
+		let diff_to_hour = stored_min_slot % 3600;
+		let current_hour_slot = stored_min_slot.saturating_sub(diff_to_hour);
+		LastTimeSlot::<T>::put((current_hour_slot, current_hour_slot));
 
 		info!(target: "automation-time", "Completed automation-time migration to v2");
 		StorageVersion::new(2).put::<Pallet<T>>();
