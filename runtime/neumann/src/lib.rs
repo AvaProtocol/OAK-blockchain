38a348c049e2d5efa5e4928c9908b6d7de36774b
Nikhil Vengal <ngvengal@gmail.com>
2022-04-19 17:37:10 -0700

    Notify ParachainStaking on block authoring

--- runtime/neumann/src/lib.rs
+++ runtime/neumann/src/lib.rs
@@ -326,7 +326,7 @@ impl pallet_authorship::Config for Runtime {
 	type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
 	type UncleGenerations = UncleGenerations;
 	type FilterUncle = ();
-	type EventHandler = (CollatorSelection,);
+	type EventHandler = ParachainStaking;
 }
 
 parameter_types! {
