8d896b97f4262ba8788a74b81ae5587397a30b9f
Justin Zhou <justinzhou@justins-mbp-2.myfiosgateway.com>
2022-04-25 16:46:07 -0400

    adjust changelog

--- runtime/neumann/src/lib.rs
+++ runtime/neumann/src/lib.rs
@@ -165,7 +165,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("neumann"),
 	impl_name: create_runtime_str!("neumann"),
 	authoring_version: 1,
-	spec_version: 279,
+	spec_version: 280,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 6,
