b840ab61cb3dd4c6a80d7868968f06257897f488
Justin Zhou <justinzhou@justins-mbp-2.myfiosgateway.com>
2022-06-16 12:40:19 -0400

    add back downward message weight var

--- runtime/neumann/src/xcm_config.rs
+++ runtime/neumann/src/xcm_config.rs
@@ -275,6 +275,10 @@ impl Config for XcmConfig {
 	type SubscriptionService = PolkadotXcm;
 }
 
+parameter_types! {
+	pub const MaxDownwardMessageWeight: Weight = MAXIMUM_BLOCK_WEIGHT / 10;
+}
+
 /// No local origins on this chain are allowed to dispatch XCM sends/executions.
 pub type LocalOriginToLocation = SignedToAccountId32<Origin, AccountId, RelayNetwork>;
 
