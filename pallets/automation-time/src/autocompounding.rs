67c61afdddf8b6702db158718083b55137b5889e
Andrew Tomaka <andrew@oak.tech>
2022-07-13 14:08:17 -0400

    Include the macro

--- pallets/automation-time/src/autocompounding.rs
+++ pallets/automation-time/src/autocompounding.rs
@@ -14,7 +14,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use sp_std::vec::Vec;
+use sp_std::{vec, vec::Vec};
 
 #[derive(Clone)]
 pub struct IntervalRow {
