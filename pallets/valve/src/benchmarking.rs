81700315b7b0f4300718ac23d4642a9f8a7e7a84
Ryan Huttman <ryan@oak.tech>
2022-09-06 11:26:50 -0700

    adding tests for benchmarks

--- pallets/valve/src/benchmarking.rs
+++ pallets/valve/src/benchmarking.rs
@@ -21,28 +21,43 @@ use super::*;
 use frame_benchmarking::{account, benchmarks};
 use frame_system::RawOrigin;
 
+fn assert_last_event<T: Config>(generic_event: <T as Config>::Event) {
+	frame_system::Pallet::<T>::assert_last_event(generic_event.into());
+}
+
 benchmarks! {
 	close_valve {
-		let account_id = account::<T::AccountId>("old member", 0, SEED);
 	}: _(RawOrigin::Root)
+	verify {
+		assert_last_event::<T>(Event::ValveClosed.into())
+	}
 
 	open_valve {
 		ValveClosed::<T>::put(true);
 	}: _(RawOrigin::Root)
+	verify {
+		assert_last_event::<T>(Event::ValveOpen.into())
+	}
 
 	close_pallet_gate_new {
-
-	}: close_pallet_gate(RawOrigin::Root, b"System".to_vec())
+		let pallet_name = b"System".to_vec();
+	}: close_pallet_gate(RawOrigin::Root, pallet_name.clone())
+	verify {
+		assert_last_event::<T>(Event::PalletGateClosed{ pallet_name_bytes: pallet_name }.into())
+	}
 
 	close_pallet_gate_existing {
-		ClosedPallets::<T>::insert(b"System".to_vec(), ());
-	}: close_pallet_gate(RawOrigin::Root, b"System".to_vec())
-
+		let pallet_name = b"System".to_vec();
+		ClosedPallets::<T>::insert(pallet_name.clone(), ());
+	}: close_pallet_gate(RawOrigin::Root, pallet_name.to_vec())
 
 	open_pallet_gate {
 		let pallet_name = b"System".to_vec();
 		ClosedPallets::<T>::insert(pallet_name.clone(), ());
-	}: _(RawOrigin::Root, pallet_name)
+	}: _(RawOrigin::Root, pallet_name.clone())
+	verify {
+		assert_last_event::<T>(Event::PalletGateOpen{ pallet_name_bytes: pallet_name }.into())
+	}
 
 	open_pallet_gates {
 		ClosedPallets::<T>::insert(b"System".to_vec(), ());
@@ -52,12 +67,21 @@ benchmarks! {
 		ClosedPallets::<T>::insert(b"Treasury".to_vec(), ());
 		ClosedPalletCount::<T>::put(5);
 	}: _(RawOrigin::Root)
+	verify {
+		assert_last_event::<T>(Event::PalletGatesClosed{ count: 0 }.into())
+	}
 
 	stop_scheduled_tasks {
 
 	}: _(RawOrigin::Root)
+	verify {
+		assert_last_event::<T>(Event::ScheduledTasksStopped.into())
+	}
 
 	start_scheduled_tasks {
 		T::AutomationTime::shutdown();
 	}: _(RawOrigin::Root)
+	verify {
+		assert_last_event::<T>(Event::ScheduledTasksResumed.into())
+	}
 }
