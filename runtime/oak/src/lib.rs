2f2fa5167dbe185bb3276fca01af436637183207
Ryan Huttman <ryan@oak.tech>
2022-09-06 16:09:08 -0700

    adding filter config to runtimes

--- runtime/oak/src/lib.rs
+++ runtime/oak/src/lib.rs
@@ -990,6 +990,17 @@ where
 	}
 }
 
+pub struct ScheduleAllowList;
+impl Contains<Call> for ScheduleAllowList {
+	fn contains(c: &Call) -> bool {
+		match c {
+			Call::System(_) => true,
+			Call::Balances(_) => true,
+			_ => false,
+		}
+	}
+}
+
 impl pallet_automation_time::Config for Runtime {
 	type Event = Event;
 	type MaxTasksPerSlot = ConstU32<256>;
@@ -1010,6 +1021,7 @@ impl pallet_automation_time::Config for Runtime {
 	type FeeHandler = pallet_automation_time::FeeHandler<DealWithExecutionFees<Runtime>>;
 	type DelegatorActions = ParachainStaking;
 	type Call = Call;
+	type ScheduleAllowList = ScheduleAllowList;
 }
 
 impl pallet_automation_price::Config for Runtime {
