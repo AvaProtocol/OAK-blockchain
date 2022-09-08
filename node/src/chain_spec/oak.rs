8ec93bbebe9e956d4889f114cf94c0d1e93724ed
laurar <lreesb1@gmail.com>
2022-09-08 16:46:14 -0400

    update oak and neumann runtimes

--- node/src/chain_spec/oak.rs
+++ node/src/chain_spec/oak.rs
@@ -149,5 +149,6 @@ fn testnet_genesis(
 		treasury: Default::default(),
 		valve: ValveConfig { start_with_valve_closed: false, closed_gates: pallet_gates_closed },
 		vesting: VestingConfig { vesting_schedule },
+		asset_registry: Default::default(),
 	}
 }
