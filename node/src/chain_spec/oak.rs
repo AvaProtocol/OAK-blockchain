5b54a98f1b0f331e8679e04a180715f6a2422146
Justin Zhou <justin@oak.tech>
2022-09-13 19:12:28 -0500

    oak live chain spec configs

--- node/src/chain_spec/oak.rs
+++ node/src/chain_spec/oak.rs
@@ -21,8 +21,10 @@ const TOKEN_SYMBOL: &str = "OAK";
 const SS_58_FORMAT: u32 = 51;
 static RELAY_CHAIN: &str = "rococo-local";
 static STAGING_RELAY_CHAIN: &str = "rococo";
+static LIVE_RELAY_CHAIN: &str = "polkadot";
 const REGISTERED_PARA_ID: u32 = 2114;
 const REGISTERED_STAGING_PARA_ID: u32 = 4103;
+const REGISTERED_LIVE_PARA_ID: u32 = 2090;
 
 /// The default XCM version to set in genesis config.
 const SAFE_XCM_VERSION: u32 = xcm::prelude::XCM_VERSION;
@@ -184,6 +186,105 @@ pub fn oak_staging() -> ChainSpec {
 		Extensions {
 			relay_chain: STAGING_RELAY_CHAIN.into(), // You MUST set this to the correct network!
 			para_id: REGISTERED_STAGING_PARA_ID,
+		},
+	)
+}
+
+pub fn oak_live() -> ChainSpec {
+	// Give your base currency a unit name and decimal places
+	let mut properties = sc_chain_spec::Properties::new();
+	properties.insert("tokenSymbol".into(), TOKEN_SYMBOL.into());
+	properties.insert("tokenDecimals".into(), TOKEN_DECIMALS.into());
+	properties.insert("ss58Format".into(), SS_58_FORMAT.into());
+
+	ChainSpec::from_genesis(
+		// Name
+		"OAK",
+		// ID
+		"oak",
+		ChainType::Live,
+		move || {
+			let allocation_json =
+				&include_bytes!("../../../distribution/oak_alloc.json")[..];
+			let initial_allocation: Vec<(AccountId, Balance)> =
+				serde_json::from_slice(allocation_json).unwrap();
+			const ALLOC_TOKENS_TOTAL: u128 = DOLLAR * 1_000_000_000;
+			validate_allocation(
+				initial_allocation.clone(),
+				ALLOC_TOKENS_TOTAL,
+				EXISTENTIAL_DEPOSIT,
+			);
+
+			let vesting_json = &include_bytes!("../../../distribution/oak_vesting.json")[..];
+			let initial_vesting: Vec<(u64, Vec<(AccountId, Balance)>)> =
+				serde_json::from_slice(vesting_json).unwrap();
+
+			let vested_tokens = 9_419_999_999_999_999_919;
+			let vest_starting_time: u64 = 1669874400;
+			let vest_ending_time: u64 = 1764568800;
+			validate_vesting(
+				initial_vesting.clone(),
+				vested_tokens,
+				EXISTENTIAL_DEPOSIT,
+				vest_starting_time,
+				vest_ending_time,
+			);
+
+			testnet_genesis(
+				// initial collators.
+				vec![
+					(
+						// SS58 prefix substrate: 5EvKT4iWQ5gDnRhkS3d254RVCfaQJSyXHj5mA4kQTsCjWchW
+						hex!["7e4f4efde71551c83714fb3062724067e7bb6ebc3bf942813321f1583e187572"]
+							.into(),
+						hex!["7e4f4efde71551c83714fb3062724067e7bb6ebc3bf942813321f1583e187572"]
+							.unchecked_into(),
+					),
+					(
+						// SS58 prefix substrate: 5F4Dx9TD16awU7FeGD3Me5VDrEsL5MDPEcbNBvUgLQFawxyW
+						hex!["8456c30af083c2b2d767d6950e8ee654d4eff5e69cdd9f75db5bcbf2d7b0d801"]
+							.into(),
+						hex!["8456c30af083c2b2d767d6950e8ee654d4eff5e69cdd9f75db5bcbf2d7b0d801"]
+							.unchecked_into(),
+					),
+				],
+				// 66MGxr9zcyJ6ka6FBQmT1VSvMqARKfwBT7589Fikii1Ci5sg
+				hex!["08df8338e854d8d589dedd4305c11e589cbef994e5dd00c7bb8fb7d277705b06"].into(),
+				initial_allocation,
+				REGISTERED_LIVE_PARA_ID.into(),
+				vec![],
+				initial_vesting,
+				vec![
+					// 69pKU2QpgtMBT9NsaN1diyJQ8qcvrJy8KJk5aWeAXfMGjb5F
+					hex!["a23443cef4fe4e7ee3f61c8248505312fa81121f2a8bd64099390b40d7a05206"].into(),
+					// 69Eyxo3gFpZpcRrtx5NAFBDMcoiVQrdg7so1M9w4FWiTBLej
+					hex!["88c782a383f0cfa5fbc36c9734dfc7cb6319137b48cc69cc662c84b6e687a106"].into(),
+					// 67D6ecyNhnAzZqgRbxr3MdGnxB9Bw8VadMhjpLAYB3wf5Pq6
+					hex!["2edf0fd8948ea642f135b314b1358c77ec6d0a4af83220b6ea18136e5ce36277"].into(),
+				],
+				vec![
+					// 69pKU2QpgtMBT9NsaN1diyJQ8qcvrJy8KJk5aWeAXfMGjb5F
+					hex!["a23443cef4fe4e7ee3f61c8248505312fa81121f2a8bd64099390b40d7a05206"].into(),
+					// 67ppUQ3mGfaWDEr5VQRz4QgvNzXiGLmGjwwEcnvbJofSQyMN
+					hex!["4a1d713c2f41e10db31b83a45a3a98b64088330190eac7fcef4623694fbac55e"].into(),
+					// 6871Utgdq5ycWnTzm7VEwQfg4zM81JqYv8EDzKPu3fdzsXK8
+					hex!["56766d3f856f2ca399ae575689a7340c2532351948f355c21bc06b170569da35"].into(),
+				],
+			)
+		},
+		// Bootnodes
+		Vec::new(),
+		// Telemetry
+		TelemetryEndpoints::new(vec![(TELEMETRY_URL.into(), 0)]).ok(),
+		// Protocol ID
+		Some("oak"),
+		None,
+		// Properties
+		Some(properties),
+		// Extensions
+		Extensions {
+			relay_chain: LIVE_RELAY_CHAIN.into(), // You MUST set this to the correct network!
+			para_id: REGISTERED_LIVE_PARA_ID,
 		},
 	)
 }
