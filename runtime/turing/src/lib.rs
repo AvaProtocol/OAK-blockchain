e40bf71a064ae0768ede87fe735723a7d198a24c
Justin Zhou <justin@oak.tech>
2022-09-14 17:18:17 -0500

    update enactment period back to 2 days

--- runtime/turing/src/lib.rs
+++ runtime/turing/src/lib.rs
@@ -924,7 +924,7 @@ parameter_types! {
 	pub const VotingPeriod: BlockNumber = 5 * DAYS;
 	pub const FastTrackVotingPeriod: BlockNumber = 1 * HOURS;
 	pub const MinimumDeposit: Balance = 40 * DOLLAR;
-	pub const EnactmentPeriod: BlockNumber = 1 * DAYS;
+	pub const EnactmentPeriod: BlockNumber = 2 * DAYS;
 	pub const VoteLockingPeriod: BlockNumber = 5 * DAYS;
 	pub const CooloffPeriod: BlockNumber = 5 * DAYS;
 	pub const InstantAllowed: bool = true;
