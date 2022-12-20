c6f3ba2af8a8767bec17f76b28642c166dfbb055
Nikhil Vengal <ngvengal@gmail.com>
2022-12-20 11:30:56 -0800

    Update runtimes

--- runtime/turing/src/xcm_config.rs
+++ runtime/turing/src/xcm_config.rs
@@ -395,6 +395,7 @@ impl pallet_xcmp_handler::Config for Runtime {
 	type XcmExecutor = XcmExecutor<XcmConfig>;
 	type Weigher = FixedWeightBounds<UnitWeightCost, Call, MaxInstructions>;
 	type WeightInfo = pallet_xcmp_handler::weights::SubstrateWeight<Runtime>;
+	type XcmFlowSelector = ();
 }
 
 pub struct TokenIdConvert;
