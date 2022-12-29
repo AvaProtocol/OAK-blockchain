c5acaa59cbbaa771700c2247e3097ef99a4ad6df
Nikhil Vengal <nikhil@oak.tech>
2022-12-29 09:38:58 -0800

    Add support for XCM flow that begins with DescendOrigin (#321)
    * Allow incoming xcm messages where fees are paid from the DescendOrigin user account
    
    * Add XcmFlowSelector trait and type
    
    * Update runtimes
    
    * Add alternate flow instruction set
    
    * Update runtimes

--- pallets/xcmp-handler/src/mock.rs
+++ pallets/xcmp-handler/src/mock.rs
@@ -270,11 +270,13 @@ impl pallet_xcmp_handler::Config for Test {
 	type GetNativeCurrencyId = GetNativeCurrencyId;
 	type SelfParaId = parachain_info::Pallet<Test>;
 	type AccountIdToMultiLocation = AccountIdToMultiLocation;
+	type CurrencyIdToMultiLocation = ();
 	type LocationInverter = LocationInverter<Ancestry>;
 	type XcmExecutor = XcmExecutor<XcmConfig>;
 	type XcmSender = TestSendXcm;
 	type Weigher = FixedWeightBounds<UnitWeightCost, Call, MaxInstructions>;
 	type WeightInfo = ();
+	type XcmFlowSelector = ();
 }
 
 // Build genesis storage according to the mock runtime.
