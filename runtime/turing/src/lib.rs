354fd2d5c5ac15a1691b7d62916698e5b1f8704c
Nikhil Vengal <nikhil@oak.tech>
2023-02-22 11:00:20 -0800

    Bump to spec version 289 (#339)

--- runtime/turing/src/lib.rs
+++ runtime/turing/src/lib.rs
@@ -168,7 +168,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("turing"),
 	impl_name: create_runtime_str!("turing"),
 	authoring_version: 1,
-	spec_version: 288,
+	spec_version: 289,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
 	transaction_version: 14,
