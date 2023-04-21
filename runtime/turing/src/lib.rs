418dfe5136c03cc3d9aed17f619b09862c6c5594
imstar15 <imstar15@qq.com>
2023-04-21 13:06:12 +0800

    Bump to spec version 291 (#353)

--- runtime/turing/src/lib.rs
+++ runtime/turing/src/lib.rs
@@ -179,10 +179,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("turing"),
 	impl_name: create_runtime_str!("turing"),
 	authoring_version: 1,
-	spec_version: 290,
+	spec_version: 291,
 	impl_version: 1,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 14,
+	transaction_version: 15,
 	state_version: 0,
 };
 
