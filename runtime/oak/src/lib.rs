7418c0b7eaaff2d5cdf5a6343d8167563b2abe18
imstar15 <imstar15@qq.com>
2023-12-01 10:06:22 +0800

    Bump to node version 2.1.0 and impl version 0(spec_version: 296)

--- runtime/oak/src/lib.rs
+++ runtime/oak/src/lib.rs
@@ -179,10 +179,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("oak"),
 	impl_name: create_runtime_str!("oak"),
 	authoring_version: 1,
-	spec_version: 295,
-	impl_version: 2,
+	spec_version: 296,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 18,
+	transaction_version: 19,
 	state_version: 0,
 };
 
