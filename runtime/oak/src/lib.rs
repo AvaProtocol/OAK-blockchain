182258c7496c22f49a498a262010369a6fb3ae6c
imstar15 <imstar15@qq.com>
2024-01-09 14:02:52 +0800

    Bump to spec version 297 (#503)

--- runtime/oak/src/lib.rs
+++ runtime/oak/src/lib.rs
@@ -179,10 +179,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: create_runtime_str!("oak"),
 	impl_name: create_runtime_str!("oak"),
 	authoring_version: 1,
-	spec_version: 296,
-	impl_version: 3,
+	spec_version: 297,
+	impl_version: 0,
 	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 19,
+	transaction_version: 20,
 	state_version: 0,
 };
 
